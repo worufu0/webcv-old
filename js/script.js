324d31672c69f8d667b8162900f9f67dc11d9575
worufu97 <orc.drahni@gmail.com>
2021-04-19 23:14:21 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+(function ($) {
+    // Preloader
+    $(window).on('load', function () {
+        if ($('.preloader').length) {
+            $('.preloader').fadeOut('slow');
+        }
+    });
+
+    // Menu
+    $('.header__hamburger-a').on('click', function () {
+        if ($('html').hasClass('html__menu-show')) {
+            $('html').removeClass('html__menu-show');
+        } else {
+            $('html').addClass('html__menu-show');
+        }
+    });
+
+    $('.menu-main__item').on('click', function () {
+        if ($('html').hasClass('html__menu-show')) {
+            $('html').removeClass('html__menu-show');
+        } else {
+            $('html').addClass('html__menu-show');
+        }
+    });
+
+    // Pagepiling
+    if ($('#pagepiling').length) {
+        $('#pagepiling').pagepiling({
+            scrollingSpeed: 280,
+            menu: '#menu, #menu-main',
+            anchors: [
+                'About',
+                'Skills',
+                'Education',
+                'Experience',
+                'Projects',
+                'Contact',
+            ],
+            loopTop: false,
+            loopBottom: false,
+            navigation: {
+                position: 'right',
+                tooltips: [
+                    'About',
+                    'Skills',
+                    'Education',
+                    'Experience',
+                    'Projects',
+                    'Contact',
+                ],
+            },
+        });
+    }
+})($);
